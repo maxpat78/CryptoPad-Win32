d469dcf2f66725d9514cd1fba23866637bb2916a
maxpat78 <maxpat78@yahoo.it>
2018-12-08 08:51:58 +0100

    Fix accelerators
    Enables CTRL+A (select all) and CTRL+U (undo) edit control accelerators

--- CryptoPad/resource.h
+++ CryptoPad/resource.h
@@ -62,7 +62,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        127
-#define _APS_NEXT_COMMAND_VALUE         40025
+#define _APS_NEXT_COMMAND_VALUE         40028
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           102
 #endif
